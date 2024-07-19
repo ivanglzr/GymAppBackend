47bb36e7cabae6b7abddba515715f0b3d97111a1
Ivan <ivanglzr01@gmail.com>
2024-07-19 17:57:59 +0200

    DB URI added

--- config.js
+++ config.js
@@ -5,5 +5,6 @@ export const statusMessages = {
 
 export const SALT_ROUNDS = 10;
 
-export const URI = "mongodb://localhost:27017/gym-app";
+export const URI =
+  "mongodb+srv://ivanglzr01:5sKSZZJ8ok8JLjyT@gymapp.p0r91bi.mongodb.net/?retryWrites=true&w=majority&appName=GymApp";
 export const PORT = 3900;
