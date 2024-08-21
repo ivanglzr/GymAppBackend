37a669579021f8be4e5707c6baa181df7857bebe
Ivan <ivanglzr01@gmail.com>
2024-08-21 22:57:41 +0200

    checkSearchParam creaed

---
+++ middlewares/checkSearchParam.js
@@ -0,0 +1,26 @@
+import lodash from "lodash";
+import { statusMessages } from "../config.js";
+import { blockIP } from "./blockIp.js";
+
+export async function checkSearchParam(req, res, next) {
+  const { search } = req.params;
+
+  if (!search) {
+    return res.status(400).json({
+      status: statusMessages.error,
+      message: "Search is required",
+    });
+  }
+
+  const sanitizedSearch = lodash.escape(search);
+
+  if (sanitizedSearch !== search) {
+    blockIP(req.ip);
+    return res.status(403).json({
+      status: statusMessages.error,
+      message: "You have been banned from the server",
+    });
+  }
+
+  next();
+}
